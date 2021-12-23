3003504d0e4a4173e0ce719f65000b8ec1c62272
deVamshi <dev.vamshidhar@gmail.com>
2021-12-24 00:32:21 +0530

    conditional rendering using a device detect package

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./App.scss";
 import AppHeader from "./components/common/AppHeader";
 import Footer from "./components/common/Footer";
 import Register from "./components/register/Register";
+import { isMobile } from "react-device-detect";
 import {
   BrowserRouter as Router,
   Route,
@@ -15,38 +16,24 @@ import Dashboard from "./dashboard-ui";
 import { useEffect, useState } from "react";
 // const { Header, Content } = Layout;
 function App() {
-  const [width, setWidth] = useState<number>(window.innerWidth);
 
-  function handleWindowSizeChange() {
-    setWidth(window.innerWidth);
-  }
-  useEffect(() => {
-    window.addEventListener("resize", handleWindowSizeChange);
-    return () => {
-      window.removeEventListener("resize", handleWindowSizeChange);
-    };
-  }, []);
-
-  const isMobile = width <= 1000;
+  if (isMobile) return <h4>Mobile view not supported(yet)</h4>;
   
-  return !isMobile ? (
-    <Router>
-      <div className="App">
-        <Affix>
-          <AppHeader />
-        </Affix>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          {/* <Route path="/transactions" exact component={Transactions}/>
+  return;
+  <Router>
+    <div className="App">
+      <Affix>
+        <AppHeader />
+      </Affix>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        {/* <Route path="/transactions" exact component={Transactions}/>
         <Route path="/matches" exact component={Matches}/>
         <Route path="/users" exact component={Users}/>
         <Route path="/produces" exact component={Produces}/> */}
-        </Routes>
-      </div>
-    </Router>
-  ) : (
-    <h4>Mobile view not supported(yet)</h4>
-  );
+      </Routes>
+    </div>
+  </Router>;
 }
 
 export default App;
